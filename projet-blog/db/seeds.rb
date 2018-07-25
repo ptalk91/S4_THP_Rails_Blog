42d6c00eb4d52873d02c0272bda66ff6fbbc164c
ptalk <pierre.lekhac@sciencespo.fr>
2018-07-25 21:03:38 +0200

    awesome blog

---
+++ projet-blog/db/seeds.rb
@@ -0,0 +1,41 @@
+require 'faker'
+
+# Creates 10 users
+10.times do
+  User.create(first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::WebWebWeb.email)
+end
+
+# Creates 10 articles
+10.times do
+    Article.create(user_id: User.all.sample.id,
+      title: Faker::QuoteLastWords.last_words,
+      content: Faker::HouseOfCards.quote)
+end
+
+# Creates 5 categories
+5.times do
+  Category.create(name: Faker::DBZ.island)
+end
+
+# Give each article a random category
+Article.all.each do |article|
+  article.category_id = Category.all.sample.id
+  article.save
+end
+
+# Create 15 comments with associated articles and users
+15.times do
+  Comment.create(user_id: User.all.sample.id,
+                article_id: Article.all.sample.id,
+                content: Faker::DBZ.quote
+                )
+end
+
+# Create 15 likes with a user and an article
+15.times do
+  Like.create(user_id: User.all.sample.id,
+              article_id: Article.all.sample.id,
+              )
+end
